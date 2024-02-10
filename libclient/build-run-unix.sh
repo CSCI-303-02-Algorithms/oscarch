d3489730f8ccd2d24babdb69df45457d2d0b9613
Oscar Chaparro <ojcch1@gmail.com>
2024-02-10 09:57:43 -0500

    Fix

--- libclient/build-run-unix.sh
+++ libclient/build-run-unix.sh
@@ -7,7 +7,7 @@ mkdir -p build-unix
 cd build-unix
 
 #cmake will check if the compiler, C++ libraries, etc. are correct in your system and generate the necessary files to compile the code
-cmake -DCMAKE_CXX_COMPILER=g++-12 -DCMAKE_C_COMPILER=gcc-12 ../
+cmake ../
 
 #cmake will compile the code and build the executable
 cmake --build ./
